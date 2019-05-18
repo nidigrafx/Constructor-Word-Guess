4dd79a8bd4287bfa19b748c05808e705e49a19df
nidigrafx <nidigrafx@gmail.com>
2019-05-18 17:56:17 -0500

    initial

---
+++ list.js
@@ -0,0 +1,11 @@
+// Used randomword npm to create list of words //
+var listOfWords = [
+    'milk', 'steam', 'steep', 'bottom', 'leaving',
+    'shot', 'harbor', 'thin', 'closely', 'coffee',
+    'lying', 'death', 'account', 'steady', 'process',
+    'equator', 'dangerous', 'task', 'hello', 'form',
+    'teacher', 'receive', 'trouble', 'setting', 'ability',
+    'rich', 'add', 'solar', 'time', 'east'
+];
+// Exports the list of words to other scripts //
+module.exports = listOfWords;
